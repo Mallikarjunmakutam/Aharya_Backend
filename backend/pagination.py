d2b1009f0f27b2884e46e5d9b250228c76298b7c
Mallikarjunmakutam <mallikarjunmakutam@gmail.com>
2026-07-20 17:08:07 +0530

    product-page

---
+++ backend/pagination.py
@@ -0,0 +1,9 @@
+from rest_framework.pagination import PageNumberPagination
+
+class OptionalPageNumberPagination(PageNumberPagination):
+    page_size_query_param = 'page_size'
+
+    def paginate_queryset(self, queryset, request, view=None):
+        if request.query_params.get('no_pagination') == 'true':
+            return None
+        return super().paginate_queryset(queryset, request, view)
